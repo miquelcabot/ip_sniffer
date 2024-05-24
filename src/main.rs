1ff7943f92e7560ab0475bf813714288176928a3
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-05-24 16:27:55 +0200

    implement scan

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::str::FromStr;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
+const MAX: u16 = 65535;
+
 #[derive(Debug)]
 struct Arguments {
-    flag: String,
     ipaddr: IpAddr,
     threads: u16,
 }
@@ -24,11 +25,7 @@ impl Arguments {
         let f = args[1].clone();
 
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
-            return Ok(Arguments {
-                flag: String::from(""),
-                ipaddr,
-                threads: 4,
-            });
+            return Ok(Arguments { ipaddr, threads: 4 });
         } else {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
@@ -48,15 +45,30 @@ impl Arguments {
                     Ok(s) => s,
                     Err(_) => return Err("failed to parse thread number"),
                 };
-                return Ok(Arguments {
-                    flag,
-                    ipaddr,
-                    threads,
-                });
+                return Ok(Arguments { ipaddr, threads });
             } else {
                 return Err("invalid syntax");
             };
         }
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
     }
 }
 
@@ -72,5 +84,24 @@ fn main() {
         }
     });
 
-    println!("args: {:?}", arguments);
+    let (tx, rx) = channel();
+    for i in 0..arguments.threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, arguments.ipaddr, arguments.threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
