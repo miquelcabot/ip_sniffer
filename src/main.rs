7169601c12585fd41c9c6fbfdd1a0f76c11fec4a
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-05-24 15:40:34 +0200

    add Arguments and read args()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    for i in &args {
+        println!("{}", i);
+    }
+
+    println!("{:?}", args);
 }
